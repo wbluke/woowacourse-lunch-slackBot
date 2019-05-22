bf4b59f825fd6b9a3ab985f20979bd61a4885997
deocksoo <dusol92@snu.ac.kr>
2019-05-22 21:34:09 +0900

    [ADD] Test caching restaurant in jumsimBot

--- jumsimbot_beta.py
+++ jumsimbot_beta.py
@@ -17,23 +17,36 @@ def say_hello(**payload):
         thread_ts = data['ts']
         user = data['user']
 
-        worksheet = Worksheet(JSON_KEYFILE_ADDRESS, SHEET_NAME)
-        restaurant = worksheet.get_restaurant(2)
         
         # web_client.chat_postMessage(channel=channel_id, text=f"Hi <@{user}>!")
-        web_client.chat_postMessage(
-            channel=channel_id,
-            blocks=[
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "<" + restaurant.get_naver_place_addr() + "|" + restaurant.get_name() + "> \n 대표 메뉴 : " + restaurant.get_popular_menu() + "  가격 : " + restaurant.get_price_of_popular_menu() + " \n :thumbsup: " + restaurant.get_good() + ":thumbsdown: " + restaurant.get_bad()
-                    }   
-                }
-            ]
-        )
+        for restaurant in restaurants:
+            web_client.chat_postMessage(
+                channel=channel_id,
+                blocks=[
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "<" 
+                            + restaurant.get_naver_place_addr() + "|" + restaurant.get_name() 
+                            + "> \n 대표 메뉴 : " + restaurant.get_popular_menu() + "  가격 : " 
+                            + restaurant.get_price_of_popular_menu() + " \n :thumbsup: " 
+                            + restaurant.get_good() + ":thumbsdown: " + restaurant.get_bad()
+                        }   
+                    }
+                ]
+            )
+
+worksheet = Worksheet(JSON_KEYFILE_ADDRESS, SHEET_NAME)
+restaurants = []
+rows = worksheet.get_all_values()
+header = rows.pop(0)   
+
+for row in rows:
+    restaurants.append(row)
 
 slack_token = os.environ["SLACK_API_TOKEN"]
 rtm_client = slack.RTMClient(token=slack_token)
 rtm_client.start()
+
+        
