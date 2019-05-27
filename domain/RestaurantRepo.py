314cdc80c236b29099ce9369639779346be4662a
wbluke <pwb0127@naver.com>
2019-05-27 20:58:18 +0900

    [ADD] RestaurantRepo에 랜덤으로 식당을 뽑아서 반환해주는 메소드 로직 추가

--- domain/RestaurantRepo.py
+++ domain/RestaurantRepo.py
@@ -1,7 +1,55 @@
+import random
+import copy
+from Queue import Queue
+
 class RestaurantRepo():
-    _retaurant_info = []
+    _restaurant_info = dict()
+    _changed_restaurants = Queue()
     
-    def __init__(self):
+    def __init__(self, gspreadClient):
+        self.gspreadClient = gspreadClient
+        self.fetch_all_restaurants()
+
+    def fetch_all_restaurants(self):
+        all_restaurants = self.gspreadClient.get_all_restaurants()
+        for restaurant in all_restaurants:
+            self._restaurant_info[restaurant.get_primary_key()] = restaurant
+
+    def get_random_recommendations_as_many_of(self, num_of_recommendation):
+        choiced_keys = self.pick_primary_keys_by_rand(self.get_primary_keys(), num_of_recommendation)
+        return [copy.deepcopy(self._restaurant_info.get(primary_key)) for primary_key in choiced_keys]
+
+    def pick_primary_keys_by_rand(self, primary_keys, num_of_recommendation):
+        print(primary_keys)
+        choiced_keys = set()
+        while len(choiced_keys) < num_of_recommendation:
+            choiced_keys.add(random.choice(primary_keys))
+        return list(choiced_keys)
+    
+    def get_primary_keys(self):
+        return list(self._restaurant_info.keys())
+
+    def upload_changed_restaurants(self):
         pass
 
-# gspread_client_task
+    def update_thumbsup(self, primary_key):
+        pass
+
+    def update_thumbsdown(self, primary_key):
+        pass
+
+
+if __name__ == "__main__":
+    # for testing this class
+
+    JSON_KEYFILE_ADDRESS = '../lunchBot-worksheet-key.json'
+    SHEET_NAME = 'woowacourse-lunch-sheet'
+
+    from GspreadClient import GspreadClient
+
+    gspreadClient = GspreadClient(JSON_KEYFILE_ADDRESS, SHEET_NAME)
+    restaurantRepo = RestaurantRepo(gspreadClient)
+
+    choiced = restaurantRepo.get_random_recommendations_as_many_of(4)
+    for restaurant in choiced:
+        print(restaurant.get_primary_key())
